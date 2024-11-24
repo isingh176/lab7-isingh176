366c2788eff7267122b07136c4c37740daa10ab4
Ishwinder Singh <isingh176@myseneca.ca>
2024-11-24 18:42:59 -0500

    Completed lab7a

--- lab7a.py
+++ lab7a.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Student ID: [seneca_id]
+# Student ID: isingh176
 class Time:
     """Simple object type for time of the day.
     data attributes: hour, minute, second
@@ -20,6 +20,15 @@ def sum_times(t1, t2):
     sum.hour = t1.hour + t2.hour
     sum.minute = t1.minute + t2.minute
     sum.second = t1.second + t2.second
+    # Carry over seconds to minutes
+    if sum.second >= 60:
+        sum.minute += sum.second // 60
+        sum.second = sum.second % 60
+
+    # Carry over minutes to hours
+    if sum.minute >= 60:
+        sum.hour += sum.minute // 60
+        sum.minute = sum.minute % 60
     return sum
 
 def valid_time(t):
